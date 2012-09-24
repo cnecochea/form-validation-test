a97a1e17d96bc1114913ed55e63a5d9a4be9e4fc
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2012-09-24 09:18:02 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormValidationTest::Application.config.secret_token = 'af1898afdcded2360eeddd99c22a9e13462c8c57eb517a406deb9c8e47c4eaf726b2904a7d5ec9ad1dee6aeb68710b6a350575a2c58db50fafd3d5f69ddbafd4'
