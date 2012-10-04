37737af164f54145fba47668d3e97ca326dc0ffb
Michael Berkowitz <michael.berkowitz@gmail.com>
2012-10-04 16:14:24 -0400

    Fewer duplicate date field errors.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -5,11 +5,11 @@ module ActionView
   module Helpers
     class FormBuilder
       def validated_date_select(field_name, options={}, html_options={})
+        options.merge!(validate: false)
         validator_id = "#{@object_name}_#{field_name}"
         html_options.merge!('data-validated-by' => validator_id,
                             'data-pre-validation-callback' => "buildDateString('#{validator_id}')")
-        @template.date_select(@object_name, field_name, options, html_options) +
-          @template.hidden_field(@object_name, field_name, 'data-validate' => true)
+        @template.date_select(@object_name, field_name, options, html_options)
       end
     end
   end
